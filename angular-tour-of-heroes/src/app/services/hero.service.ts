674ef5ced56f992c0eff38ad3dd38c5ac285b518
SergiOn <Onishhenko@gmail.com>
2017-07-05 22:53:31 +0300

    #7-http # Add the ability to delete a hero

--- angular-tour-of-heroes/src/app/services/hero.service.ts
+++ angular-tour-of-heroes/src/app/services/hero.service.ts
@@ -49,6 +49,14 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
   // getHeroes(): Promise<Hero[]> {
   //   return Promise.resolve(HEROES);
   // }
